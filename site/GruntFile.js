e25b0018da34cb4ab1a80bccef55d40ff625e43f
Jean Amarante <jeanamarantec@gmail.com>
2013-12-01 00:12:47 -0500

    {Update} GruntFile.js now supports file structure

--- site/GruntFile.js
+++ site/GruntFile.js
@@ -1,15 +1,40 @@
 module.exports = function (grunt) {
 	
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks("grunt-contrib-clean");
 	grunt.loadNpmTasks("grunt-contrib-uglify");
-	grunt.registerTask('min', 'uglify');
+	grunt.loadNpmTasks("grunt-html2js");
+	
+	grunt.registerTask("build", ["clean", "html2js", "concat", "uglify"]);
 	
 	grunt.initConfig({
+		html2js: {
+			app: {
+				src: ["src/**/*.tpl.html"],
+				dest: "src/build/templates/app-tpl.js"
+			}
+		},
 		uglify: {
 			dist: {
-				src: ["src/*.js"],
-				dest: "dist/avalon.min.js"
+				src: ["src/build/avalon-app.js"],
+				dest: "dist/avalon-app-min.js"
 			}
-		}
+		},
+		concat: {
+			dist: {
+				src: ["src/**/*.js"],
+				dest: "src/build/avalon-app.js"
+			},
+			index: {
+				src: ["src/index.html"],
+				dest: "dist/index.html"
+			},
+			angular: {
+				src: ["vendor/angular/angular.min.js"],
+				dest: "dist/vendor/angular.js"
+			}
+		},
+		clean: ["dist/*", "src/build/*"]
 	});
 	
 };
