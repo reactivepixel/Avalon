8015e3ab3aed670a1e8e7a80324134bb6ec37f6d
Jean Amarante <jeanamarantec@gmail.com>
2013-12-17 14:07:17 -0400

    [Delivers #61928360] {Chore} Allow user to see other people's content.

--- site/src/app/content/artists.js
+++ site/src/app/content/artists.js
@@ -1,3 +1,57 @@
-function ArtistsCtrl() {
+function ArtistsCtrl($scope, FBURL, $sce) {
     
+    $scope.content = [];
+    
+    $scope.searchArtist = function () {
+        $scope.content = [];
+        searchUser($scope.artistName);
+    };
+    
+    function searchUser(user) {
+        new Firebase(FBURL+"users/")
+            .once("value", function (snapshot) {
+                var id = snapshot.forEach(function (child) {
+                    if (child.val().username === user) {
+                        return child.name();
+                    }
+                });
+                getData(id);
+            });
+    }
+    
+    function getData(id) {
+        new Firebase(FBURL+"content/"+id)
+            .once("value", function (snapshot) {
+                snapshot.forEach(function (child) {
+    
+                    var data = child.val();
+                    
+                    if (data.type === "youtube" || data.type === "vimeo") {
+                        $scope.$apply(function () {
+                            if (data.type === "youtube") {
+                                data.embed = "<iframe width='640' height='360' src='//www.youtube.com/embed/"+data.url+"?rel=0' frameborder='0' allowfullscreen></iframe>";
+                            } else if (data.type === "vimeo") {
+                                data.embed = "<iframe src='//player.vimeo.com/video/"+data.url+"' width='640' height='360' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+                            } 
+                            $scope.content.push(data);
+                        });
+                    }
+                    
+                    if (data.type === "track") {
+                        SC.oEmbed(data.url, { auto_play: false }, function(oEmbed) {
+                            $scope.$apply(function () {
+                                data.embed = oEmbed.html;
+                                $scope.content.push(data);
+                            });
+                        });
+                    }
+                });
+            });
+    }
+    
+    $scope.render = function (text) {
+        if (text !== undefined) {
+            return $sce.trustAsHtml(text);
+        }
+    };
 }
